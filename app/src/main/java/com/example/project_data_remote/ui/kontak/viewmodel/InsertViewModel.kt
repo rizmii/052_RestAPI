b5d22155332f79ed4aaedae8495365a2dceb02b0
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2023-12-27 11:42:09 +0700

    add InsertViewModel.kt

--- app/src/main/java/com/example/project_data_remote/ui/kontak/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/project_data_remote/ui/kontak/viewmodel/InsertViewModel.kt
@@ -1,2 +1,63 @@
 package com.example.project_data_remote.ui.kontak.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.project_data_remote.model.Kontak
+import com.example.project_data_remote.repository.KontakRepository
+import com.example.project_data_remote.ui.home.viewmodel.InsertUiEvent
+import com.example.project_data_remote.ui.home.viewmodel.InsertUiState
+import com.example.project_data_remote.ui.home.viewmodel.toKontak
+import kotlinx.coroutines.launch
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+
+    suspend fun insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val email: String = "",
+    val nohp: String = ""
+)
+
+fun InsertUiEvent.toKontak(): Kontak = Kontak(
+    id = id,
+    nama = nama,
+    alamat = email,
+    nohp = no_hp
+)
+
+
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent(),
+    // Other properties you might want to include
+)
+
+fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    id = id,
+    nama = nama,
+    email = alamat,
+    no_hp = nohp
+)
