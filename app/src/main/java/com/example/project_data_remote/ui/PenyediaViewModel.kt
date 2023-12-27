a178084860b4e9c89371e49fbe0b08254621c603
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2023-12-27 11:43:11 +0700

    add InsertViewModel.kt to PenyediaViewModel.kt

--- app/src/main/java/com/example/project_data_remote/ui/PenyediaViewModel.kt
+++ app/src/main/java/com/example/project_data_remote/ui/PenyediaViewModel.kt
@@ -6,13 +6,16 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.project_data_remote.KontakApplication
 import com.example.project_data_remote.ui.home.viewmodel.HomeViewModel
+import com.example.project_data_remote.ui.kontak.viewmodel.InsertViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             HomeViewModel(apkikasiMars().container.kontakRepository)
         }
-
+        initializer {
+            InsertViewModel(apkikasiMars().container.kontakRepository)
+        }
     }
 }
 
