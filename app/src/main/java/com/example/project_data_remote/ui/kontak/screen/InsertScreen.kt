fb8011b78007e9f77a81bcc68cf93b20b5b5f136
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2023-12-27 12:02:01 +0700

    add EntryKontakScreen

--- app/src/main/java/com/example/project_data_remote/ui/kontak/screen/InsertScreen.kt
+++ app/src/main/java/com/example/project_data_remote/ui/kontak/screen/InsertScreen.kt
@@ -4,19 +4,33 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.project_data_remote.navigation.DestinasiNavigasi
+import com.example.project_data_remote.ui.PenyediaViewModel
+import com.example.project_data_remote.ui.TopAppBarKontak
 import com.example.project_data_remote.ui.home.viewmodel.InsertUiEvent
 import com.example.project_data_remote.ui.home.viewmodel.InsertUiState
+import com.example.project_data_remote.ui.kontak.viewmodel.InsertViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 //import com.google.mlkit.vision.text.Text
 
@@ -97,6 +111,48 @@ fun EntryKontakBody(
         ) {
             Text(text = "Simpan")
         }
+    }
+}
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Siswa"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryKontakScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory),
+
+) { val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBarKontak(
+                title =  DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+
+        EntryKontakBody(
+            insertUiState = viewModel.insertKontakState,
+            onSiswaValueChange = { viewModel.updateInsertKontakState(it) },
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertKontak()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
     }
 }
 
