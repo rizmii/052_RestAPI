a7b8d088442a18b43812150394c5f79cb46b4199
khawarizmi  azis septian <khawarizmi.azis.ft21@mail.umy.ac.id>
2023-12-28 17:08:26 +0700

    add PengelolaHalaman.kt

--- app/src/main/java/com/example/project_data_remote/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project_data_remote/navigation/PengelolaHalaman.kt
@@ -1,4 +1,40 @@
 package com.example.project_data_remote.navigation
 
-class PengelolaHalaman {
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.example.project_data_remote.ui.home.screen.DestinasiHome
+import com.example.project_data_remote.ui.home.screen.HomeScreen
+import com.example.project_data_remote.ui.kontak.screen.DestinasiEntry
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                onDetailClick = {
+                    // Define the action when the detail is clicked
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            EntryKontakScreen(
+                navigateBack = {
+                    navController.navigate(DestinasiHome.route) {
+                        popUpTo(DestinasiHome.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+    }
 }
